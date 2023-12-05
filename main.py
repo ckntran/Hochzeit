a780729a7b9986da3a81a5f1629eae2e59508551
Christian Tran <christian.kn.tran@gmail.com>
2023-12-05 06:46:02 +0100

    fixed KeyError

--- main.py
+++ main.py
@@ -9,14 +9,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 Bootstrap5(app)
 
-BEARER_TOKEN = os.environ.get('SHEETY_BEARER_TOKEN')
-
 sheety_endpoint = os.environ.get('SHEETY_ENDPOINT')
-
-#Bearer Token Authentication
-bearer_headers = {
-    "Authorization": f'Bearer {BEARER_TOKEN}'
-}
 
 @app.route("/")
 @app.route("/<lang>")
@@ -45,28 +38,27 @@ def rsvp(lang=None):
         form = GuestForm_tw()
 
     if form.validate_on_submit():
-        #response = requests.get(url=sheety_endpoint, headers=bearer_headers).json()['guests']
-        print(requests.get(url=sheety_endpoint, headers=bearer_headers).json())
-        #data = [guest for guest in response if form.first_name.data.lower().strip() in guest['firstName'].lower().strip() and form.last_name.data.lower().strip() in guest['lastName'].lower().strip()]
-        #if len(data) > 0:
-        #    return render_template("rsvp.html", form=form, data=data, double_entry=True, lang=lang)
-        #else:
-        sheety_parameters = {
-            "guest": {
-                "firstName": form.first_name.data,
-                "lastName": form.last_name.data,
-                "email": form.email.data,
-                "willYouBeAttending?": form.attendance.data,
-                "willYouBeBringingAGuest?": form.plus_one.data,
-                "guestFirstName": form.guest_first_name.data,
-                "guestLastName": form.guest_last_name.data,
-                "anyDietaryRestrictions?": form.dietary_restriction.data,
-                "questionsOrComments?": form.comment.data,
-                "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        response = requests.get(url=sheety_endpoint).json()['guests']
+        data = [guest for guest in response if form.first_name.data.lower().strip() in guest['firstName'].lower().strip() and form.last_name.data.lower().strip() in guest['lastName'].lower().strip()]
+        if len(data) > 0:
+            return render_template("rsvp.html", form=form, data=data, double_entry=True, lang=lang)
+        else:
+            sheety_parameters = {
+                "guest": {
+                    "firstName": form.first_name.data,
+                    "lastName": form.last_name.data,
+                    "email": form.email.data,
+                    "willYouBeAttending?": form.attendance.data,
+                    "willYouBeBringingAGuest?": form.plus_one.data,
+                    "guestFirstName": form.guest_first_name.data,
+                    "guestLastName": form.guest_last_name.data,
+                    "anyDietaryRestrictions?": form.dietary_restriction.data,
+                    "questionsOrComments?": form.comment.data,
+                    "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                }
             }
-        }
-        requests.post(url=sheety_endpoint, json=sheety_parameters, headers=bearer_headers)
-        return render_template("rsvp.html", form=form, submission_successful=True, lang=lang)
+            requests.post(url=sheety_endpoint, json=sheety_parameters)
+            return render_template("rsvp.html", form=form, submission_successful=True, lang=lang)
     return render_template("rsvp.html", form=form, lang=lang)
 
 @app.route("/the-wedding-day")
